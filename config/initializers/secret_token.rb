e95f57acad4ecb5f080624db88051a7aff81f5bd
Matthew Mongeau <matt@toastyapps.com>
2010-09-21 22:42:15 -0400

    Generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinchlist::Application.config.secret_token = '9c96dad2b812fa0674a1402b7882c2a0c362f80069843075cb6c072bfe3f3c4c53a1a6ab723cd424f04ef2f683a7f8ca07601e75ab670118913057f7fd5bf113'
