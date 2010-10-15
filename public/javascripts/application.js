328b96d8256b9914a5d74ebdb27507570686c190
Benjamin Lehn <ben@Benjamin-Lehns-MacBook-Pro.local>
2010-10-15 14:12:11 -0400

    adjusted column resizing and header positioning

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// $(document).ready(function(){
+//   var maxWidth = 300;    
+//   if ( $('.list').width() > maxWidth ) $('.list').width(maxWidth);
+// });
