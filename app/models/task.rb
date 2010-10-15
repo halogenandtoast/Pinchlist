fa459ed5db5380cf7ec54ea0b6413ff008fbc829
Matthew Mongeau <matt@toastyapps.com>
2010-10-14 20:59:39 -0400

    Support date anywhere in title

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,21 @@
 class Task < ActiveRecord::Base
   belongs_to :list
+  DATE_PATTERN = /@(\d{1,2}\/\d{1,2})/
 
   def title=(title)
-    if title =~ /@(\d{1,2}\/\d{1,2}) (.*)/
-      title = $2
-      day, month = $1.split('/').map(&:to_i)
-      write_attribute(:due_date, Date.new(Time.now.year, month, day))
+    if title =~ DATE_PATTERN
+      write_attribute(:due_date, parse_date_format($1))
     end
-    write_attribute(:title, title)
+    write_attribute(:title, remove_date_format(title))
+  end
+
+  private
+
+  def parse_date_format(str)
+    Date.new(Time.now.year, *str.split('/').map(&:to_i).reverse)
+  end
+
+  def remove_date_format(str)
+    str.gsub(%r{(\s+#{DATE_PATTERN}|#{DATE_PATTERN}\s+)}, '')
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,7 +4,22 @@ describe Task do
   it { should belong_to :list }
 end
 
-describe Task, 'title with due date' do
-  subject { Factory(:task, :title => "@20/10 Build foo") }
-  its(:due_date) { should == "20/10/#{Time.now.year}".to_date }
+describe Task, 'whose title has a due date' do
+  context 'in the front' do
+    subject { Factory(:task, :title => "@20/10 Build foo") }
+    its(:due_date) { should == "20/10/#{Time.now.year}".to_date }
+    its(:title) { should == 'Build foo' }
+  end
+
+  context 'in the back' do
+    subject { Factory(:task, :title => "Build foo @20/10") }
+    its(:due_date) { should == "20/10/#{Time.now.year}".to_date }
+    its(:title) { should == 'Build foo' }
+  end
+
+  context 'in the middle' do
+    subject { Factory(:task, :title => "Build @20/10 foo") }
+    its(:due_date) { should == "20/10/#{Time.now.year}".to_date }
+    its(:title) { should == 'Build foo' }
+  end
 end
