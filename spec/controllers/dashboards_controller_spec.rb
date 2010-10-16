f88f2ed9896283fbd56f94eef969fc4f2d394b21
Matthew Mongeau <matt@toastyapps.com>
2010-10-16 16:35:17 -0400

    Test that dashboard redirects.

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe DashboardsController, 'routes' do
   it { should route(:get, '/dashboard').to(:action => 'show') }
+
+  it 'should redirect if not signed in' do
+    get 'show'
+    response.should be_redirect
+  end
 end
