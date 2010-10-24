5fb393b0153154b4626eb14faa46036b21c68f41
Matthew Mongeau <matt@toastyapps.com>
2010-10-24 10:57:55 -0400

    Deleting a list should delete the tasks.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
   scope :by_task_status, order("lists.created_at ASC, tasks.completed ASC, tasks.created_at ASC")
   scope :current_tasks, lambda { where(["(tasks.completed_at IS NULL OR tasks.completed_at > ?)", 7.days.ago.to_date]) }
 end
