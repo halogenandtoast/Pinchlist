31dbba6d026ef482b83a84ac1f1cbd1d69f951c3
Matthew Mongeau <matt@toastyapps.com>
2010-11-24 00:07:27 -0500

    Fix colors. Clear out new list field when list is created.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,11 +1,18 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :tasks, :dependent => :destroy
+  before_create :set_color
   scope :by_task_status, order("lists.position ASC, tasks.completed ASC, tasks.position ASC")
   scope :current_tasks, lambda { where(["(tasks.completed_at IS NULL OR tasks.completed_at > ?)", 7.days.ago.to_date]) }
   acts_as_list :scope => :user
 
   def new_position=(position)
     self.insert_at(position)
+  end
+
+  private
+
+  def set_color
+    self.color = "%06x" % (rand * 0xffffff)
   end
 end
