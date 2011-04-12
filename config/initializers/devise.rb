473b74de61d3f0a2cb2c4712611cf98fa9449e24
Matthew Mongeau <matt@toastyapps.com>
2011-04-12 00:42:51 -0400

    Little more personal

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "donotreply@listwerk.com"
+  config.mailer_sender = "Listwerk <donotreply@listwerk.com>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
