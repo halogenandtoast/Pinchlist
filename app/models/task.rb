76dceee7450ae4e1e54ed94a608144372a40b709
Matthew Mongeau <matt@toastyapps.com>
2011-10-22 14:49:05 -0400

    Use word boundaries for date matcher.

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ActiveRecord::Base
   MONTH_DAY_FORMAT = /(\d{1,2}\/\d{1,2})/
   DAYNAMES = Date::DAYNAMES+Date::ABBR_DAYNAMES+['tues','thurs','thur','tod','today','tom','tomorrow']
   MONTHNAMES = (Date::MONTHNAMES+Date::ABBR_MONTHNAMES).compact
-  REMOVE_DATE_MATCHER = / ((on|next) )?(((#{DAYNAMES.join("|")}))|((#{MONTHNAMES.join("|")})( \d{1,2})?)|(\d{1,2}\/\d{1,2}(\/\d{2}\d{2}?)?))/i
+  REMOVE_DATE_MATCHER = /\b((on|next) )?(((#{DAYNAMES.join("|")}))|((#{MONTHNAMES.join("|")})( \d{1,2})?)|(\d{1,2}\/\d{1,2}(\/\d{2}\d{2}?)?))\b/i
 
   belongs_to :list
   validates_presence_of :title, :on => :create
