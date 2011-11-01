07da3b15f95d97468909b980f43004fa76231d44
Matthew Mongeau <matt@toastyapps.com>
2011-11-01 00:15:08 -0400

    Update session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Pinchlist::Application.config.session_store :cookie_store, key: '_pinchlist_session'
+Pinchlist::Application.config.session_store :cookie_store, key: '_listwerk_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
