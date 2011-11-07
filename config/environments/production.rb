bd84bd2c08d12ef760f8934f7ead666bf137969f
Matthew Mongeau <matt@toastyapps.com>
2011-11-07 08:43:40 -0500

    Force SSL again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Pinchlist::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  # config.middleware.use Rack::SSL, host: "www.listwerk.com"
+  config.middleware.use Rack::SSL, host: "www.listwerk.com"
 
   # Code is not reloaded between requests
   config.cache_classes = true
