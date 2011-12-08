f6aa40b2c92ccbff77f16eb3ee4ea0cf55c04b8f
Matthew Mongeau <halogenandtoast@gmail.com>
2011-12-08 13:12:17 -0500

    Configure sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,4 +59,13 @@ Pinchlist::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { :host => 'listwerk.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
