f76026e304293fbe97d1d34c187bbde5f1b553d9
Matthew Mongeau <matt@toastyapps.com>
2012-01-11 21:20:53 -0500

    Move subscription login into module. Make permanent users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,43 +9,10 @@ class User < ActiveRecord::Base
   has_many :list_proxies
   has_many :lists, :through => :list_proxies, :readonly => false
   has_many :owned_lists, :class_name => "List"
-  has_many :subscriptions
 
   def proxy_for(list_or_id)
     list_id = list_or_id.is_a?(List) ? list_or_id.id : list_or_id
     list_proxies.find_by_list_id(list_id)
-  end
-
-  def current_subscription
-    subscriptions.current.first
-  end
-
-  def active?
-    current_subscription && current_subscription.active?
-  end
-
-  def cancelled?
-    current_subscription && current_subscription.cancelled?
-  end
-
-  def cancel_subscription!
-    current_subscription.cancel!
-  end
-
-  def resubscribe!
-    current_subscription.resubscribe!
-  end
-
-  def next_bill_date
-    current_subscription.ends_at
-  end
-
-  def expiration_date
-    current_subscription.ends_at
-  end
-
-  def subscribed?
-    current_subscription
   end
 
   def tasks
