72e522d4d86ca404be616fad14b8e27900bfea90
Matthew Mongeau <matt@toastyapps.com>
2012-02-11 10:50:48 -0500

    Control staging with flag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Pinchlist::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.middleware.use Rack::SSL, host: "www.listwerk.com"
+  if ENV['STAGING'] != "true"
+    config.middleware.use Rack::SSL, host: "www.listwerk.com"
+  end
 
   # Code is not reloaded between requests
   config.cache_classes = true
