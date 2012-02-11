c3f221c70c48dd15fb4c3df034b1f2c94ff013f9
Matthew Mongeau <matt@toastyapps.com>
2012-02-11 11:30:23 -0500

    Locked proxies need to be an empty array when proxies don't exist.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,9 +6,11 @@ class DashboardsController < ApplicationController
     @list_proxies = current_user.list_proxies.by_position.includes(:list)
     @upcoming_tasks = current_user.tasks.upcoming.current.includes(:list)
 
-    unless current_user.subscribed?
+    if !current_user.subscribed? && @list_proxies.length > List::SUBSCRIBED_LIMIT
       @locked_proxies = @list_proxies[3..-1].map { |proxy| LockedListProxy.new(proxy) }
       @list_proxies = @list_proxies[0..2]
+    else
+      @locked_proxies = []
     end
   end
 end
