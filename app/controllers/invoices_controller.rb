96b9218963a36f3a2d0e56ab924adb6ecc08e5a3
Matthew Mongeau <matt@toastyapps.com>
2012-05-20 17:10:11 -0400

    Use amount_due not amount

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,7 +5,7 @@ class InvoicesController < ApplicationController
     if json["type"] == "invoice.created"
       invoice_id = json["data"]["object"]["id"]
       stripe_customer_token = json["data"]["object"]["customer"]
-      amount = json["data"]["object"]["amount"]
+      amount = json["data"]["object"]["amount_due"]
       user = User.find_by_stripe_customer_token(stripe_customer_token)
       if user.has_credit?
         credit = [user.available_credit, amount].min
