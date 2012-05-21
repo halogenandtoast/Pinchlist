38498571fbdb6dd1e5c0fe06c4c120043a381986
Matthew Mongeau <matt@toastyapps.com>
2012-05-20 23:50:51 -0400

    Add keys as env variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,3 @@
 require 'stripe'
-Stripe.api_key = "PgGIhUisLf5d8DHEN2QX6ao9CqElykMj"
-STRIPE_PUBLIC_KEY = "pk_e2MJGVT05hufLihHAGpC9IT4S0p7y"
+Stripe.api_key = ENV['STRIPE_API_KEY'] || "PgGIhUisLf5d8DHEN2QX6ao9CqElykMj"
+STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY'] || "pk_e2MJGVT05hufLihHAGpC9IT4S0p7y"
