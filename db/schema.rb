3e256ad2477887c5036ea0dc6a29948127a437c1
Benjamin Lehn <ben.lehn@gmail.com>
2012-05-21 14:03:49 -0400

    public list styling, changed cursor paths

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,9 @@ ActiveRecord::Schema.define(:version => 20120521163830) do
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "",                 :null => false
     t.string   "encrypted_password",                  :default => ""
+    t.string   "password_salt",                       :default => ""
     t.string   "reset_password_token"
+    t.string   "remember_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
