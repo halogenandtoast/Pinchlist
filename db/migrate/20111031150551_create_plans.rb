636c3dd21cd2223f07530c016bc34266f00dd889
Matthew Mongeau <matt@toastyapps.com>
2012-05-20 23:36:18 -0400

    Add form back

--- db/migrate/20111031150551_create_plans.rb
+++ db/migrate/20111031150551_create_plans.rb
@@ -1,10 +1,10 @@
 class CreatePlans < ActiveRecord::Migration
   def change
-    # create_table :plans do |t|
-    #   t.string :name
-    #   t.decimal :price
+    create_table :plans do |t|
+      t.string :name
+      t.decimal :price
 
-    #   t.timestamps
-    # end
+      t.timestamps
+    end
   end
 end
--- db/migrate/20111031150628_add_stripe_to_subscriptions.rb
+++ db/migrate/20111031150628_add_stripe_to_subscriptions.rb
@@ -1,5 +1,5 @@
 class AddStripeToSubscriptions < ActiveRecord::Migration
   def change
-    # add_column :subscriptions, :stripe_customer_token, :string
+    add_column :subscriptions, :stripe_customer_token, :string
   end
 end
