7029a41ad088679ace93cbe90a40301b5daac570
Benjamin Lehn <ben.lehn@gmail.com>
2012-05-21 12:32:55 -0400

    darker hint text

--- app/assets/javascripts/jquery.form_labels.js
+++ app/assets/javascripts/jquery.form_labels.js
@@ -31,7 +31,7 @@
     $('select').live('change', toggleLabel);
 
     $('input, textarea').live('focusin', function() {
-        $(this).prev('span').css('color', '#F0E9C1');
+        $(this).prev('span').css('color', '#E2D264');
     });
     $('input, textarea').live('focusout', function() {
         $(this).prev('span').css('color', '#eee');
