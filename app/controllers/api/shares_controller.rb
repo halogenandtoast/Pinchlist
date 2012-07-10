c86d0b851e51995a8071e21e60366839ff06784c
Matthew Mongeau <matt@toastyapps.com>
2012-07-09 21:30:51 -0400

    Only add valid shares.

--- app/controllers/api/shares_controller.rb
+++ app/controllers/api/shares_controller.rb
@@ -4,8 +4,11 @@ class Api::SharesController < Api::BaseController
     list = current_user.lists.find(params[:list_id])
     if list.owned_by?(current_user)
       share = Share.new(params[:share].merge(list_id: list.id, current_user_id: current_user.id))
-      share.save
-      render json: share
+      if share.save
+        render json: share
+      else
+        render json: share, status: :unprocessible_entity
+      end
     end
   end
 
