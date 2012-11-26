990ab1db8636372be51fe5d702fb620fdaf76deb
halogenandtoast <halogenandtoast@gmail.com>
2012-11-26 16:30:39 -0500

    Handle public urls

--- app/controllers/public/lists_controller.rb
+++ app/controllers/public/lists_controller.rb
@@ -1,6 +1,6 @@
 class Public::ListsController < ApplicationController
   layout "public"
   def show
-    @list_proxy = ListProxy.public_from_params(params)
+    @list = List.public_from_params(params)
   end
 end
