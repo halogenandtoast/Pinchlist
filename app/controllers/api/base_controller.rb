dc83ab03b797bf7a1e6b2f0515f14c7b30fd5bc7
halogenandtoast <halogenandtoast@gmail.com>
2014-04-23 09:50:42 -0400

    Authenticate with token

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,16 @@
 class Api::BaseController < ApplicationController
   before_filter :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    if params[:token]
+      token = AuthToken.new(params[:token])
+      unless token.valid?
+        render :json, status: :unauthorized
+      end
+    else
+      super
+    end
+  end
 end
