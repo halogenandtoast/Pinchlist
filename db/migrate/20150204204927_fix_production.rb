45ca5311b167cdbcc5b4f4d55abe1ad4d30f3045
halogenandtoast <halogenandtoast@gmail.com>
2015-02-04 15:50:32 -0500

    Missing fields

---
+++ db/migrate/20150204204927_fix_production.rb
@@ -0,0 +1,10 @@
+class FixProduction < ActiveRecord::Migration
+  def change
+    unless column_exists? :users, :reset_password_sent_at
+      add_column :users, :reset_password_sent_at, :datetime
+    end
+    unless column_exists? :users, :invitation_created_at
+      add_column :users, :invitation_created_at, :datetime
+    end
+  end
+end
