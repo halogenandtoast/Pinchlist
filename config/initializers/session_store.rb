dcd465042bde5be392570d0ceac8bde8eb2c632c
Matthew Mongeau <halogenandtoast@gmail.com>
2018-06-27 13:17:38 +0900

    Updated to Rails 5

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Pinchlist::Application.config.session_store :cookie_store, key: '_listwerk_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Pinchlist::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_pinchlist_session'
